ab8959510df45d14e0b9a5b7854b92f64bc31bdf
RafPrataham <160498230+RafPrataham@users.noreply.github.com>
2024-03-11 20:06:09 +0700

    Rafi just add new word (EPIC)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 meme_dict = {
             "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
             "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "EPIC": "Sesuatu hal yang sangat keren"
             }
 
 word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ") #menginput kata yang diinginkan
